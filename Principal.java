8c71c4644e65b80528342c60e6ef18a38e6d72d0
robertocompanyzestudios <robertocompanyzestudios@gmail.com>
2025-02-05 18:56:34 +0100

    Roberto Company - Practica4_1

--- Principal.java
+++ Principal.java
@@ -1,6 +1,18 @@
-public Principal{
-  public static voi main(String[] args){
-    System.Out.println("Hola Mundo);
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
-  }
+public class Principal {
+
+    public static String getTime() {
+        
+      LocalDateTime time = LocalDateTime.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        
+        return ahora.format(format);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Fecha y hora: " + getTime());
+    }
 }
+
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Práctica 4.1 - Uso básico de Git
 
 **Descripción:** Aprendiendo a usar los comandos y utulidades básicas de git.
+
+* **Archivo Principal.Java:** Muestra la hora actual
